a28d33465321f7809ae4ea7f478f84c5fe84eb0f
Alisher Abdugaliyev <Alisher Abdugaliyev>
2016-01-29 12:44:18 +0600

    Min Prime from input.txt to output.txt

---
+++ InOutPrimes/InOutPrimes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InOutPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fs2 = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader rs = new StreamReader(fs);
+            StreamWriter sw = new StreamWriter(fs2);
+            int min = 99999999;
+            int k = 0; 
+            string[] arr = rs.ReadLine().Split(' ');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[i] = int.Parse(arr[i]);
+                
+            }
+            /*foreach (string s in arr)
+            {
+                int custom = int.Parse(s);
+            }*/
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j < 999999; j++)
+                {
+                    if (array[i] % j == 0)
+                    {
+                        k++;
+                    }
+                }
+                if (k == 2 && array[i]<min)
+                {
+                    min = array[i];
+                }
+                k = 0;
+            }
+
+            sw.WriteLine("\n" + "Minimum Prime is: " + min);
+            sw.Close();
+            rs.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InOutPrimes/InOutPrimes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("InOutPrimes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InOutPrimes")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("89752194-224b-400e-9b77-ba5165f7b89e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
