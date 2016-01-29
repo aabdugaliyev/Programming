8f532ad0a7164c6b498ee2310028b73da900f9ab
Alisher Abdugaliyev <Alisher Abdugaliyev>
2016-01-29 12:39:49 +0600

    Max Min from input.txt

---
+++ InOut/InOut/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[50];
+            FileStream fs1 = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            string[] arr = sr.ReadLine().Split(' ');
+            int max = -1;
+            int min = 999999999;
+            
+            foreach (string s in arr)
+            {
+                int custom = int.Parse(s);
+                if (custom > max)
+                {
+                    max = custom;
+                }
+                if (custom < min)
+                {
+                    min = custom;
+                }
+            }
+
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+
+            Console.ReadKey();
+        }
+    }
+}
