1fbf6102ec5c136cfe3b2f3a4880e6073493f4a3
Alisher Abdugaliyev <Alisher Abdugaliyev>
2016-01-30 10:58:37 +0600

    Prime Numbers

---
+++ PrimeNum/PrimeNum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primes = new int[50];
+            int[] prime = new int[50];
+            int k = 0;
+            int p = 0;
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < s.Length; i++)
+            {
+                prime[i] = int.Parse(s[i]);
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 1; j < 99999; j++)
+                {
+                    if (prime[i] % j == 0)
+                    {
+                        p++;
+                    }
+                }
+                if (p == 2)
+                {
+                    primes[k] = prime[i];
+                    k++;
+                }
+                p = 0;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(primes[i]);
+            }
+
+                Console.ReadKey();
+
+        }
+    }
+}
